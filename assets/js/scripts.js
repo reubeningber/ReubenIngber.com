7b690b72a722aef1e724e491ab6315485e2844a2
Reuben Ingber <reuben.ingber@expandtheroom.com>
2016-01-31 12:19:45 -0500

    Various updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,21 +25,5 @@
 // });
 
 $(document).ready(function () {
-  // $('.accordion-tabs').each(function() {
-  //   $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
-  // });
-  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
-
-      $(this).next().toggleClass('is-open').toggle();
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-      window.location.replace($('[data-page-name]');
-    } else {
-      event.preventDefault();
-    }
-  });
+  
 });
