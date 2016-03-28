e5fd61770a286539f067d0164dcc7db1912b86db
Reuben Ingber <reuben.ingber@expandtheroom.com>
2016-03-27 22:32:04 -0400

    Fix #21

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 	    get: 'user',
         userId: '1920699',
 	    clientId: '4bbfbc0356b24be5bb42f8bebcf178a9',
-	    template: '<li class="instgram-feed__item"><a href="{{link}}"><img src="{{image}}" /></a></li>',
+	    template: '<li class="instagram-feed__item"><a href="{{link}}"><img src="{{image}}" /></a></li>',
 	    limit: 5
 	});
 	feed.run();
@@ -16,15 +16,15 @@ $(document).ready(function () {
 	/** Twitter */
 	/*-------------------------------------------- */
 
-	var twitterConfig = {
-		"id": '694661085845151744',
-		"domId": 'tw-widget',
-		"maxTweets": 8,
-		"enableLinks": true,
-		"showImages": false,
-		"showUser": false,
-		"showInteraction": false
-	};
+	// var twitterConfig = {
+	// 	"id": '694661085845151744',
+	// 	"domId": 'tw-widget',
+	// 	"maxTweets": 8,
+	// 	"enableLinks": true,
+	// 	"showImages": false,
+	// 	"showUser": false,
+	// 	"showInteraction": false
+	// };
 
-	twitterFetcher.fetch(twitterConfig);
+	// twitterFetcher.fetch(twitterConfig);
 });
